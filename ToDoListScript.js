09a9bab87e8b750436e0136a952c6a6791634e01
maanu-1 <61041467+maanu-1@users.noreply.github.com>
2020-02-14 14:10:23 +0530

    ToDoApp(HTML,CSS,js)

---
+++ ToDoListScript.js
@@ -0,0 +1,57 @@
+
+
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	myNodelist[i].appendChild(span);
+}
+
+// Close button to close item in the todo list
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+	close[i].onclick = function () {
+		var div = this.parentElement;
+		div.style.display = "none";
+	}
+}
+
+// Striking a line against an item which is completed
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+	if (ev.target.tagName === 'LI') {
+		ev.target.classList.toggle('checked');
+	}
+}, false);
+
+
+// Adding a new item in ToDo list
+function newElement() {
+	var li = document.createElement("li");
+	var inputValue = document.getElementById("myInput").value;
+	var t = document.createTextNode(inputValue);
+	li.appendChild(t);
+	if (inputValue === '') {
+		alert("You must write something!");
+	} else {
+		document.getElementById("myUL").appendChild(li);
+	}
+	document.getElementById("myInput").value = "";
+
+	var span = document.createElement("SPAN");
+	var txt = document.createTextNode("\u00D7");
+	span.className = "close";
+	span.appendChild(txt);
+	li.appendChild(span);
+
+	for (i = 0; i < close.length; i++) {
+		close[i].onclick = function () {
+			var div = this.parentElement;
+			div.style.display = "none";
+		}
+	}
+}
